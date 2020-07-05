6e29aa3033786d892f742c7bd92bc1545c52e7a9
Paul <paul@links123.com>
2020-07-06 00:35:51 +0800

    as sub command

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/wpajqz/xwc/cmd"
+	"github.com/wpajqz/xwc/config"
 )
 
 func main() {
@@ -14,7 +19,60 @@ func main() {
 	}
 
 	rootCmd.AddCommand(cmd.RunInitCommand())
-	rootCmd.AddCommand(cmd.RunCallCommand())
+
+	c := &config.Config{}
+	if err := c.LoadConfigFile("xwc.yml"); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for _, element := range c.Enviroment {
+		for k, v := range element {
+			if err := os.Setenv(k, v); err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+		}
+	}
+
+	for k, v := range c.Command {
+		rootCmd.AddCommand(
+			&cobra.Command{
+				Use:   k,
+				Short: fmt.Sprintf("Exec: %s", v),
+				Long:  fmt.Sprintf("Exec: %s", v),
+				Run: func(cmd *cobra.Command, args []string) {
+					var param []string
+
+					ss := strings.Split(v, " ")
+					if len(ss) > 1 {
+						param = append(param, ss[1:]...)
+					}
+
+					param = append(param, args...)
+
+					for k, v := range param {
+						param[k] = os.ExpandEnv(v)
+					}
+
+					fmt.Printf("exec: %s %s\n", ss[0], strings.Join(param, " "))
+
+					var ob, eb bytes.Buffer
+					execCmd := exec.Command(ss[0], param...)
+					execCmd.Stdout = &ob
+					execCmd.Stderr = &eb
+
+					err := execCmd.Run()
+					if err != nil {
+						fmt.Println(eb.String())
+						return
+					}
+
+					fmt.Println(ob.String())
+				},
+			},
+		)
+	}
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err.Error())
